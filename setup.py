ad2fe5804f11a28591ba6a96a9f2aa1f098f8348
Robert Mele <rmorlando@gmail.com>
2015-07-27 09:35:52 -0700

    basic setup file structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='net_tools',
+      version='0.1',
+      description='Network Tools',
+      author='Robert Mele',
+      py_modules=['main'],
+      # author_email='rmorlando@gmail.com',
+      # url='https://www.wintermute.com',
+      # packages=['net_tools', ],
+      )

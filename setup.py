0e2d5b4ca9d8d1186e70ce8570147a05ef630936
Robert Mele <rmorlando@gmail.com>
2015-09-23 16:22:19 -0700

    improve setup preparing for py2app

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 setup(name='net_tools',
       version='0.1',
       description='Network Tools',
       author='Robert Mele',
-      py_modules=['main'],
-      # author_email='rmorlando@gmail.com',
-      # url='https://www.wintermute.com',
-      # packages=['net_tools', ],
+      author_email='rmorlando@gmail.com',
+      py_modules=['net_tools'],
+      url='https://www.wintermute.com',
+      zip_safe=False,
       )

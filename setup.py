a5d0f746a9fbf0648cab925c9bbc54b39a4fd7ac
mrmele <rmorlando@gmail.com>
2018-01-24 10:59:26 -0800

    Adding setup file

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ setup(name='net_tools',
       version='0.1',
       description='Network Tools',
       author='Robert Mele',
-      author_email='rmorlando@gmail.com',
       py_modules=['net_tools'],
-      url='https://www.wintermute.com',
       zip_safe=False,
       )
